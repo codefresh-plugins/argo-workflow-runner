9bb7a26b759e2636295f8aeb833fdba51b919af8
pashavictorovich <pavel@codefresh.io>
2021-06-28 18:30:46 +0300

    change a way how to export env variable

--- src/infra/env-exporter.js
+++ src/infra/env-exporter.js
@@ -3,7 +3,7 @@ const exec = require('util').promisify(require('child_process').exec);
 class EnvExporter {
 
     async export(key,  value) {
-        const { stderr } = await exec(`echo ${key}=${value} >> /codefresh/volume/env_vars_to_export`);
+        const { stderr } = await exec(`echo ${key}=${value} >> /meta/env_vars_to_export`);
         if (stderr) {
             throw new Error(`Error occurred while executing cf-export: ${stderr}`);
         } else {
